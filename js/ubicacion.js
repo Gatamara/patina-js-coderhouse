866d7dbdcad2cadf3f641747c43f7266645215f1
Tamara Salinas <gatamara29@hmail.com>
2022-12-03 00:33:01 -0300

    fix

--- js/ubicacion.js
+++ js/ubicacion.js
@@ -1,45 +1,41 @@
-const elementosDestacados = [ {
-        name: "Parque los Reyes",
-        type: "Skatepark",
-        city: "Santiago",
-        filename: "../assets/img/parque-losreyes.jpg",
-        obstaculos: [ "baranda", "escalera", "bowl", "banca", "halfpipe" ],
-        latitud: -33.425708132244544,
-        longitud: -70.66626039478183,
-    },
-    {
-        name: "Parque Ohiggins",
-        type: "Skatepark",
-        city: "Santiago",
-        filename: "../assets/img/parque-ohiggins.jpg",
-        obstaculos: [ "baranda", "piramide", "bowl", "banca" ],
-        latitud: -33.463027371223305,
-        longitud: -70.6574253306844,
-    }, {
-        name: "Parque Bustamante",
-        type: "Skatepark",
-        city: "Santiago",
-        filename: "../assets/img/parque-bustamante.jpg",
-        obstaculos: [ "baranda", "bowl", "muro", "halfpipe" ],
-        latitud: -33.441507832224765,
-        longitud: -70.63150533611231,
-    },
-
-
-]
-
-
-
+const elementosDestacados = [
+  {
+    name: "Parque los Reyes",
+    type: "Skatepark",
+    city: "Santiago",
+    filename: "assets/img/parque-losreyes.jpg",
+    obstaculos: ["baranda", "escalera", "bowl", "banca", "halfpipe"],
+    latitud: -33.425708132244544,
+    longitud: -70.66626039478183,
+  },
+  {
+    name: "Parque Ohiggins",
+    type: "Skatepark",
+    city: "Santiago",
+    filename: "assets/img/parque-ohiggins.jpg",
+    obstaculos: ["baranda", "piramide", "bowl", "banca"],
+    latitud: -33.463027371223305,
+    longitud: -70.6574253306844,
+  },
+  {
+    name: "Parque Bustamante",
+    type: "Skatepark",
+    city: "Santiago",
+    filename: "assets/img/parque-bustamante.jpg",
+    obstaculos: ["baranda", "bowl", "muro", "halfpipe"],
+    latitud: -33.441507832224765,
+    longitud: -70.63150533611231,
+  },
+];
 
 //////////////////////////////////////////////////////////////////////////
 // Card de Spots Destacados
 
-const mostrarDestacados = elementosDestacados => {
+const mostrarDestacados = (elementosDestacados) => {
+  $("#spots-destacados").html("");
 
-    $( '#spots-destacados' ).html( "" );
-
-    for ( const elmentoDestacado of elementosDestacados ) {
-        $( '#spots-destacados' ).prepend( `
+  for (const elmentoDestacado of elementosDestacados) {
+    $("#spots-destacados").prepend(`
 
         <div class="spot-item">
                 <div class="spot-content p-1">
@@ -59,26 +55,23 @@ const mostrarDestacados = elementosDestacados => {
                 </div>
             </div>
     
-    ` )
-
-
-    }
-}
-mostrarDestacados( elementosDestacados );
+    `);
+  }
+};
+mostrarDestacados(elementosDestacados);
 
 /////////////////////////////////////////////////////////////////////////////
 
-
 function iniciarMapp() {
-    let coord = { lat: -33.463027371223305, lng: -70.6574253306844 };
-    let map = new google.maps.Map( document.getElementById( 'map' ), {
-        zoom: 15,
-        center: coord
-    } );
-    let marker = new google.maps.Marker( {
-        position: coord,
-        map: map
-    } );
+  let coord = { lat: -33.463027371223305, lng: -70.6574253306844 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: coord,
+  });
+  let marker = new google.maps.Marker({
+    position: coord,
+    map: map,
+  });
 }
 
-iniciarMapp()
+iniciarMapp();
